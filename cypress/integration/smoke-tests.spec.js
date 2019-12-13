9fd021ad97305530150197fe841453541924f8a0
Lawrence Velasquez <law.vzmk@gmail.com>
2019-12-13 08:47:39 -0600

    Add tests for full end-to-end (part 2)

--- cypress/integration/smoke-tests.spec.js
+++ cypress/integration/smoke-tests.spec.js
@@ -6,7 +6,7 @@ describe('Smoke tests', () => {
   });
 
   context('With no todos', () => {
-    it.only('Saves new todos', () => {
+    it('Saves new todos', () => {
       const items = [
         { text: 'Buy Milk', expectedLength: 1 },
         { text: 'Buy Eggs', expectedLength: 2 },
@@ -28,6 +28,66 @@ describe('Smoke tests', () => {
     
           cy.get('.todo-list li')
             .should('have.length', item.expectedLength);
+        });
+    });
+  });
+
+  context('With active todos', () => {
+    beforeEach(() => {
+      cy.fixture('todos')
+        .each(todo => {
+          const newTodo = Cypress._.merge(todo, { isComplete: false });
+          cy.request('POST', '/api/todos', newTodo);
+        });
+      cy.visit('/');
+    });
+
+    it('Loads existing data from DB', () => {
+      cy.get('.todo-list li')
+        .should('have.length', 4);
+    });
+
+    it('Delete todos', () => {
+      cy.server();
+      cy.route('DELETE', '/api/todos/*')
+        .as('delete');
+
+      cy.get('.todo-list li')
+        .each($el => {
+          cy.wrap($el)
+            .find('.destroy')
+            .invoke('show')
+            .click();
+
+          cy.wait('@delete');
+        })
+        .should('not.exist');
+    });
+
+    it('Toggle todos', () => {
+      const clickAndWait = ($el) => {
+        cy.wrap($el)
+          .as('item')
+          .find('.toggle')
+          .click();
+
+        cy.wait('@update');
+      };
+
+      cy.server();
+      cy.route('PUT', '/api/todos/*')
+        .as('update');
+
+      cy.get('.todo-list li')
+        .each($el => {
+          clickAndWait($el);
+          cy.get('@item')
+            .should('have.class', 'completed');
+        })
+        .each($el => {
+          clickAndWait($el);
+          cy.get('@item')
+            .should('not.have.class', 'completed');
         });
     });
   });
